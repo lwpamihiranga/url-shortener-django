ff820fe386c2cede824608d3b781f4981f03a808
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-05 00:21:31 +0530

    Add unit tests for RedirectView

--- url_shortener/tests.py
+++ url_shortener/tests.py
@@ -26,3 +26,30 @@ class ShortenUrlViewTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+
+class RedirectViewTests(APITestCase):
+    def setUp(self):
+        self.shortned_url = ShortenedUrl.objects.create(
+            original_url="http://example.com/",
+            short_code="aabbcc",
+            access_count=0,
+        )
+
+    def test_redirect_valid_short_code(self):
+        url = reverse("redirect_url", kwargs={"short_code": "aabbcc"})
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(response.url, "http://example.com/")
+
+        self.shortned_url.refresh_from_db()
+        self.assertEqual(self.shortned_url.access_count, 1)
+
+    def test_redirect_invalid_short_code(self):
+        url = reverse("redirect_url", kwargs={"short_code": "nothing"})
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
