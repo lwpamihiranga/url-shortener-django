b2e3a6f82c2fe8a41c965bdd54a9c7279aa59c2b
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-04 23:52:34 +0530

    Update stats url name

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -5,5 +5,5 @@ from .views import ShortenUrlView, StatsView
 urlpatterns = [
     # Add your app's URL patterns here
     path('shorten/', ShortenUrlView.as_view(), name="shorten_url"),
-    path('stats/<str:short_code>/', StatsView.as_view(), name="stats-url"),
+    path('stats/<str:short_code>/', StatsView.as_view(), name="stats_url"),
 ]
