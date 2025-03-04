befb080bf148d5c71ecc4f63b5fc9d038dcdfd0a
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-04 20:35:14 +0530

    Add get url path for urlpatterns

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from url_shortener.views import RedirectView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('short/<str:short_code>/', RedirectView.as_view(), name="redirect-url"),
     path("api/", include("url_shortener.urls")),
 ]
