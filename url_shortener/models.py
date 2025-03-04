6e0276c5c03bede271f14358d7b8b8e0f0bf9c37
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-04 19:38:17 +0530

    Create model for store url details

---
+++ url_shortener/models.py
@@ -0,0 +1,34 @@
+import hashlib
+import base64
+
+from django.db import models
+
+
+def generate_short_code(url):
+    """Generate the short code for the URL"""
+
+    hash_digest = hashlib.sha256(url.encode()).digest()
+    short_code = base64.urlsafe_b64encode(hash_digest)[:6].decode("utf-8")
+
+    counter = 1
+    while ShortenedUrl.objects.filter(short_code=short_code).exists():
+        short_code = base64.urlsafe_b64encode(
+            hash_digest)[:5].decode("utf-8") + str(counter)
+        counter += 1
+
+    return short_code
+
+
+class ShortenedUrl(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    original_url = models.URLField(unique=True)
+    short_code = models.CharField(max_length=10, unique=True, blank=True)
+    access_count = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.short_code:
+            self.short_code = generate_short_code(self.original_url)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.short_code} -> {self.original_url}"
