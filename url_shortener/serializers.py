d0e0df5bf0ee19a97175c2afd1b8e246e0398c2a
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-04 23:13:42 +0530

    Add last_accessed field for StatsSerializer

--- url_shortener/serializers.py
+++ url_shortener/serializers.py
@@ -11,4 +11,5 @@ class ShortenedUrlSerializer(serializers.ModelSerializer):
 class StatsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShortenedUrl
-        fields = ["original_url", "short_code", "access_count"]
+        fields = ["original_url", "short_code",
+                  "access_count", "last_accessed"]
