e8390069ec4b9f6119ff27dbdb4fee18fc114ca9
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-04 20:34:07 +0530

    Implement Redirect API View

--- url_shortener/views.py
+++ url_shortener/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404, redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -31,5 +32,13 @@ class ShortenUrlView(APIView):
             )
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RedirectView(APIView):
+    def get(self, request, short_code):
+        url_obj = get_object_or_404(ShortenedUrl, short_code=short_code)
+        url_obj.access_count += 1
+        url_obj.save()
+        return redirect(url_obj.original_url)
 
 
