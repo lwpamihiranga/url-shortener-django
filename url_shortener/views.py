423b4e0ca0478cc29b61590788db23adb0cc1c7d
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-05 02:32:35 +0530

    Make redirection return permanent moved

--- url_shortener/views.py
+++ url_shortener/views.py
@@ -43,7 +43,7 @@ class RedirectView(APIView):
         url_obj = get_object_or_404(ShortenedUrl, short_code=short_code)
         url_obj.access_count += 1
         url_obj.save()
-        return redirect(url_obj.original_url)
+        return redirect(url_obj.original_url, permanent=True)
 
 
 class StatsView(APIView):
