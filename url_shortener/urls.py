0276dea68606b705fb580920660f5e2ace58ccbd
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-04 20:47:00 +0530

    Add stats url for urlpatterns

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # Add your app's URL patterns here
     path('shorten/', views.ShortenUrlView.as_view(), name="shorten_url"),
+    path('stats/<str:short_code>/', views.StatsView.as_view(), name="stats-url"),
 ]
