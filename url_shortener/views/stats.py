cfef40d942f0186aa44e3f86b10dacccb33ca902
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-05 15:38:57 +0530

    Add logging statements for StatsView

--- url_shortener/views/stats.py
+++ url_shortener/views/stats.py
@@ -1,4 +1,4 @@
-
+import logging
 from django.shortcuts import get_object_or_404
 from rest_framework.throttling import AnonRateThrottle
 from rest_framework.views import APIView
@@ -8,11 +8,20 @@ from rest_framework import status
 from url_shortener.serializers.stats_serializer import StatsSerializer
 from url_shortener.models import ShortenedUrl
 
+logger = logging.getLogger(__name__)
+
 
 class StatsView(APIView):
     throttle_classes = [AnonRateThrottle]
 
     def get(self, request, short_code):
+        logger.info("Received request for stats of short code: %s", short_code)
+
         url_obj = get_object_or_404(ShortenedUrl, short_code=short_code)
         serializer = StatsSerializer(url_obj)
+
+        logger.info("Returning stats for short code: %s", short_code)
+        logger.info("Original URL: %s", url_obj.original_url)
+        logger.info("Access Count: %d | Last Accessed: %s",
+                    url_obj.access_count, url_obj.last_accessed)
         return Response(serializer.data, status=status.HTTP_200_OK)
