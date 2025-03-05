9571f01288ad7a7fd1bd263d05bf6eaf0838f3ab
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-05 10:18:20 +0530

    Remove obselete test case

--- url_shortener/tests.py
+++ url_shortener/tests.py
@@ -15,17 +15,6 @@ class ShortenUrlViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIn("short_url", response.data)
 
-    def test_duplicate_url(self):
-        ShortenedUrl.objects.create(
-            original_url="http://example.com/", short_code="aabbcc")
-
-        url = reverse("shorten_url")
-        data = {"original_url": "http://example.com/"}
-
-        response = self.client.post(url, data)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
 
 class RedirectViewTests(APITestCase):
     def setUp(self):
