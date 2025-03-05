f1c4d33498ca0be79a4eee9de5cb1b889aab02d3
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-05 10:57:29 +0530

    Remove unnecessary comment

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -4,7 +4,6 @@ from .views.stats import StatsView
 from .views.shorten_url import ShortenUrlView
 
 urlpatterns = [
-    # Add your app's URL patterns here
     path('shorten/', ShortenUrlView.as_view(), name="shorten_url"),
     path('stats/<str:short_code>/', StatsView.as_view(), name="stats_url"),
 ]
