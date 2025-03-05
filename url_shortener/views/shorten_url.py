18be25ab04c8193eb5e2dd77983e6c7f72da31d8
Amith Mihiranga <lwpamihiranga@gmail.com>
2025-03-05 15:37:58 +0530

    Add logging statements for ShortenUrlView

--- url_shortener/views/shorten_url.py
+++ url_shortener/views/shorten_url.py
@@ -1,3 +1,4 @@
+import logging
 from rest_framework.throttling import AnonRateThrottle
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -5,18 +6,26 @@ from rest_framework import status
 
 from url_shortener.serializers.url_serializer import ShortenedUrlSerializer
 
+logger = logging.getLogger(__name__)
+
 
 class ShortenUrlView(APIView):
     throttle_classes = [AnonRateThrottle]
 
     def post(self, request):
+        logger.info("Received request to shorten URL: %s", request.data)
+
         serializer = ShortenedUrlSerializer(data=request.data)
         if serializer.is_valid():
             url_instance = serializer.save()
-            return Response(
-                {"short_url": request.build_absolute_uri(
-                    f"/short/{url_instance.short_code}/")},
-                status=status.HTTP_201_CREATED
-            )
+            short_url = request.build_absolute_uri(
+                f"/short/{url_instance.short_code}/")
 
+            logger.info("Successfully created short URL: %s -> %s",
+                        url_instance.original_url, short_url)
+
+            return Response({"short_url": short_url},
+                            status=status.HTTP_201_CREATED)
+
+        logger.warning("Invalid URL shortening request: %s", serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
